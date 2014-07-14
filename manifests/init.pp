f55d621b75937a65c910a4fecd5ad095b721bff6
Gildas Cherruel <gildas.cherruel@inin.com>
2014-07-14 16:51:19 +0900

    Turned off SQL Express for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -48,27 +48,27 @@ class sqlserver(
   $sql_source  = 'http://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPR_x64_ENU.exe'
   $sql_install = url_parse($sql_source, 'filename')
 
-  exec {'sqlserver-install-download':
-    command  => "((new-object net.webclient).DownloadFile('${sql_source}','${core::cache_dir}/${sql_install}'))",
-    creates  => "${core::cache_dir}/${sql_install}",
-    provider => powershell,
-    require  => [
-                  File["${core::cache_dir}"],
-                ]
-  }
+  #  exec {'sqlserver-install-download':
+  #    command  => "((new-object net.webclient).DownloadFile('${sql_source}','${core::cache_dir}/${sql_install}'))",
+  #    creates  => "${core::cache_dir}/${sql_install}",
+  #    provider => powershell,
+  #    require  => [
+  #                  File["${core::cache_dir}"],
+  #                ]
+  #  }
 
 
-  exec {'sqlserver-install-run':
-    command  => "${core::cache_dir}/${sql_install} /Q /IACCEPTSQLSERVERLICENSETERMS /ACTION=install /FEATURES=SQL,AS,RS,IS,Tools /INSTANCENAME=\"MSSQLSERVER\" /SECURITYMODE=SQL /SAPWD=\"${sa_password}\" /TCPENABLED=1",
-    creates  => "C:/Program Files/Microsoft SQL Server/MSSQL11.MSSQLSERVER/MSSQL/binn/sqlservr.exe",
-    cwd      => "${core::cache_dir}",
-    provider => windows,
-    timeout  => 900,
-    require  => [
-                  File["${core::cache_dir}"],
-                  Exec['sqlserver-install-download'],
-                ]
-  }
+  #  exec {'sqlserver-install-run':
+  #    command  => "${core::cache_dir}/${sql_install} /Q /IACCEPTSQLSERVERLICENSETERMS /ACTION=install /FEATURES=SQL,AS,RS,IS,Tools /INSTANCENAME=\"MSSQLSERVER\" /SECURITYMODE=SQL /SAPWD=\"${sa_password}\" /TCPENABLED=1",
+  #    creates  => "C:/Program Files/Microsoft SQL Server/MSSQL11.MSSQLSERVER/MSSQL/binn/sqlservr.exe",
+  #    cwd      => "${core::cache_dir}",
+  #    provider => windows,
+  #    timeout  => 900,
+  #    require  => [
+  #                  File["${core::cache_dir}"],
+  #                  Exec['sqlserver-install-download'],
+  #                ]
+  #  }
 
   #TODO: Open the firewall for the TCP connection to SQL Server
 }
