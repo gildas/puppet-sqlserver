5c1883b568cde3e099da6661560e394336236e5b
Gildas Cherruel <gildas.cherruel@inin.com>
2014-11-13 18:28:30 +0900

    Finished options for Express and added documentation

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 sqlserver
 =========
 
-## Description
+Description
+-----------
 
 Microsoft SQL Server package installer for PuppetLabs.
 
@@ -36,16 +37,98 @@ You can install other editions via:
 class {'sqlserver':
   edition      => 'standard',
   license_type => 'Retail'
-  license      => ''
+  license      => 'H0Y11-FA8KE-L1CEN-SE890-AHAH3',
 }
 ```
 
 Valid editions are: express, standard, enterprise.
 Valid license types are: evaluation, MSDN, Volume, Retail.
 
-Notes:
-- Express edition ignores the license type and the license,
-- MSDN, Volume, and Retail license types require a license,
+> Express edition ignores the license type and the license,
+
+> MSDN, Volume, and Retail license types require a license
+
+See the next paragraph for a full list of options.
+
+### Options
+The class supports the following options (alphabetical order):
+
+- administrators
+  This option contains the DOMAIN\USER, DOMAIN\GROUP used for the Administrators of this SQL Server instance.
+  + Possible Values: *undef*, Windows NT account identifier.
+  + Default value: *undef*
+- backup_dir
+  This option specifies the directory for backup files.
+  If *undef*, the backup folder will be: *InstallSQLDataDir*/*SQLInstanceID*/MSSQL/Backup
+  + Possible Values: *undef*, folder
+  + Default value: *undef*
+- collation
+  This option specifies the collation for this SQL Server instance
+  + Possible Values: *undef*, collation identifier
+  + Default value: *undef*
+- database_dir
+  This option tells where the database data files should be stored.
+  If not provided, this will be: *InstallSQLDataDir*/*SQLInstanceID*/MSSQL/Data
+  + Possible Values: *undef*, folder
+  + Default value: *undef*
+- database_log_dir
+  This option tells where the database data files should be stored.
+  If not provided, this will be: *InstallSQLDataDir*/*SQLInstanceID*/MSSQL/Data
+  + Possible Values: *undef*, folder
+  + Default value: *undef*
+- edition
+  This option tells which SQL Server Edition will be installed.
+  + Possible Values: Express, Standard, Enterprise.
+  + Default value: Express
+- ensure
+  This option tells if the module should install SQL Server or uninstall it.
+  + Possible Values: installed, uninstalled.
+  + Default value: installed
+- features
+  This is an array of features to install. This option is case-independent.
+  + Possible values: SQL, Analysis Services, Integration Services, Reporting Services, Tools.
+  + Default value: SQL
+- force_english
+  When true, installs SQL Server in English on a localized OS with a localized install package.
+  + Possible Values: true, false
+  + Default value: false
+- instance_dir
+  This option tells where instance-specific components should be installed.
+  + Possible Values: undef, folder path.
+  + Default value: *undef*
+- instance_name
+  This option contains the name of the instance to install.
+  + Possible Values: *string*
+  + Default value: MSSQLSERVER
+- language
+  This is the language to install software with. This option is case-independent.
+  + Possible values: de, en, es, fr, ja, ko, pt, ru, zh-CHS, zh-CHT.
+  + Default value: en.
+- license
+  This options contains the license key from Microsoft.
+  The value is ignored for SQL Express and for the Evaluation edition.
+  + Possible Values: *undef*, *string*
+  + Default value: *undef*
+- license_type
+  This option contains the type of license to use. This option is ignored when installing SQL Express.
+  + Possible Values: Evaluation, MSDN, Volume, Retail.
+  + Default value: Evaluation
+- sa_password
+  If provided, SQL Server will be installed with Windows and SQL authentication. If not, Windows authentication only will be used.
+  + Possible Values: *undef*, *string*
+  + Default value: *undef*
+- show_progress
+  This option controls if a progress bar should be displayed while installing SQL Server
+  + Possible Values: true, false.
+  + Default value: **false**
+- source
+  This option specifies the path of the ISO containing the SQL Server installation.
+  This option is ignored when installing SQL Express.
+  + Possible Values: *undef*, folder
+  + Default value: *undef*
+
+Notes and Caveats:
+> By default only SQL is installed! Do not forget to add the feature 'Tools' to get Management Studio and other SQL tools.
 
 License
 -------
--- manifests/init.pp
+++ manifests/init.pp
@@ -37,19 +37,21 @@
 #
 class sqlserver(
   $ensure           = installed,
-  $edition          = 'edition',
+  $edition          = 'express',
   $license_type     = 'evaluation',
   $license          = undef,
   $language         = 'en',
   $features         = [ 'SQL' ],
+  $sa_password      = undef,
   $instance_name    = 'MSSQLSERVER',
   $instance_dir     = undef,
-  $sa_password      = undef,
   $database_dir     = undef,
   $database_log_dir = undef,
   $backup_dir       = undef,
   $collation        = undef,
   $administrators   = undef,
+  $show_progress    = false,
+  $force_english    = false,
   $source           = undef,
 )
 {
@@ -58,6 +60,9 @@ class sqlserver(
     err("This module works on Windows only!")
     fail("Unsupported OS")
   }
+  validate_re($language, ['ˆ(?i)(de|en|es|fr|ja|ko|pt|ru|zh-CHS|zh-CHT)'])
+  validate_array($features)
+  if (empty($features)) { fail("Unable to continue processing SQL Server since no features were selected") }
   validate_re($edition, ['^(?i)(express|standard|enterprise)$'])
   unless ($edition =~ /^(?i:express)$/)
   {
@@ -68,7 +73,6 @@ class sqlserver(
     }
   }
 
-
   case $ensure
   {
     installed:
@@ -78,30 +82,59 @@ class sqlserver(
       {
         'express':
         {
-          # SQL Server 2014 Express
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe'
+          case $language
+          {
+            /^(?i:de)$/:     { $install_language = 'DEU'; $source_path = '9/9/B/99BB8518-C818-42EF-A9AA-1A06E4AC1DC6' } # German
+            /^(?i:en)$/:     { $install_language = 'ENU'; $source_path = 'E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB' } # English
+            /^(?i:es)$/:     { $install_language = 'ESN'; $source_path = 'C/A/3/CA36A732-59EC-4CEA-971A-0269B992C82A' } # Spanish
+            /^(?i:fr)$/:     { $install_language = 'FRA'; $source_path = 'B/8/5/B856F507-1B8A-4A5F-BCBF-ABEE9C70CA84' } # French
+            /^(?i:ja)$/:     { $install_language = 'JPN'; $source_path = '1/C/9/1C95EAB0-F98C-4039-8402-4D7A84D9B290' } # Japanese
+            /^(?i:ko)$/:     { $install_language = 'KOR'; $source_path = '5/9/9/5995FEA5-97E8-4A01-BDFB-78E27F4873AC' } # Korean
+            /^(?i:pt)$/:     { $install_language = 'PTB'; $source_path = '0/1/5/015567C0-E851-4AC6-964F-9BBA9B31D6BC' } # Portuguese
+            /^(?i:ru)$/:     { $install_language = 'RUS'; $source_path = '4/E/3/4E38FD5A-8859-446F-8C58-9FC70FE82BB1' } # Russian
+            /^(?i:zh-CHS)$/: { $install_language = 'CHS'; $source_path = 'C/5/A/C5ACFA2B-9DB0-44F3-BD2F-BBC567987C82' } # Simplified Chinese
+            /^(?i:zh-CHT)$/: { $install_language = 'CHT'; $source_path = '5/5/E/55EA61C3-4CED-455F-B09F-67608D27BEB6' } # Traditional Chinese
+            default:
+            {
+              warn("Invalid installation language \"${language}\", defaulting to English")
+              $install_language = 'ENU'
+            }
+          }
 
-          # SQL Server 2014 Express with tools
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAndTools%2064BIT/SQLEXPRWT_x64_ENU.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/B/8/5/B856F507-1B8A-4A5F-BCBF-ABEE9C70CA84/ExpressAndTools%2064BIT/SQLEXPRWT_x64_FRA.exe'
-
-          # SQL Server 2014 Management Studio (64b)
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/1/C/9/1C95EAB0-F98C-4039-8402-4D7A84D9B290/MgmtStudio%2064BIT/SQLManagementStudio_x64_JPN.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/B/8/5/B856F507-1B8A-4A5F-BCBF-ABEE9C70CA84/MgmtStudio%2064BIT/SQLManagementStudio_x64_FRA.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/9/9/B/99BB8518-C818-42EF-A9AA-1A06E4AC1DC6/MgmtStudio%2064BIT/SQLManagementStudio_x64_DEU.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/C/A/3/CA36A732-59EC-4CEA-971A-0269B992C82A/MgmtStudio%2064BIT/SQLManagementStudio_x64_ESN.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/C/5/A/C5ACFA2B-9DB0-44F3-BD2F-BBC567987C82/MgmtStudio%2064BIT/SQLManagementStudio_x64_CHS.exe' # Chinese Simplified
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/5/5/E/55EA61C3-4CED-455F-B09F-67608D27BEB6/MgmtStudio%2064BIT/SQLManagementStudio_x64_CHT.exe' # Chinese Traditionnal
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/5/9/9/5995FEA5-97E8-4A01-BDFB-78E27F4873AC/MgmtStudio%2064BIT/SQLManagementStudio_x64_KOR.exe'
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/0/1/5/015567C0-E851-4AC6-964F-9BBA9B31D6BC/MgmtStudio%2064BIT/SQLManagementStudio_x64_PTB.exe' # Portuguese
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/4/E/3/4E38FD5A-8859-446F-8C58-9FC70FE82BB1/MgmtStudio%2064BIT/SQLManagementStudio_x64_RUS.exe'
-
-          # SQL Server 2014 with Advanced Services
-          $sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAdv%2064BIT/SQLEXPRADV_x64_ENU.exe'
-
-          $sql_source  = 'http://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPR_x64_ENU.exe'
-          $sql_install = url_parse($sql_source, 'filename')
+          if (member(downcase($features), 'analysis services') or member(downcase($features), 'integration services') or member(downcase($features), 'reporting services'))
+          {
+            $feature_AS      = member(downcase($features), 'analysis services')    ? { true => ',AS',    default => '' }
+            $feature_AS      = member(downcase($features), 'integration services') ? { true => ',IS'   , default => '' }
+            $feature_AS      = member(downcase($features), 'reporting services')   ? { true => ',RS',    default => '' }
+            $feature_tools   = member(downcase($features), 'tools')                ? { true => ',Tools', default => '' }
+            $features_option = "/FEATURES=SQL${feature_AS}${feature_IS}${feature_RS}$(feature_tools}"
+            $product_path    = 'ExpressAdv%2064BIT'
+            $sql_install     = "SQLEXPRADV_x64_${install_language}.exe"
+          }
+          else if (member(downcase($features), 'tools'))
+          {
+            $features_option = '/FEATURES=SQL,Tools'
+            $product_path    = 'ExpressAndTools%2064BIT'
+            $sql_install     = "SQLEXPRWT_x64_${install_language}.exe"
+          }
+          else
+          {
+            $features_option = '/FEATURES=SQL'
+            $product_path    = 'Express%2064BIT'
+            $sql_install     = "SQLEXPR_x64_${install_language}.exe"
+          }
+          $sql_source            = "http://care.dlservice.microsoft.com/dl/download/${source_path}/${product_path}/${sql_install}"
+          $silent_option         = $show_progress             ? { true => "/QS", default => "/Q" }
+          $enu_option            = $force_english             ? { true => "/ENU", default => '' }
+          $instance_name_option  = empty($instance_name)      ? { true => "/INSTANCENAME=\"MSSQLSERVER\"", default => "/INSTANCENAME=\"${instance_name}\"" }
+          $instance_dir_option   = empty($instance_dir)       ? { true => '', default => "/INSTANCEDIR=\"${instance_dir}\"" }
+          $security_option       = empty($sa_password)        ? { true => '', default => "/SECURITYMODE=SQL /SAPWD=\"${sa_password}\"" }
+          $database_dir_option   = empty($database_dir)       ? { true => '', default => "/SQLUSERDBDIR=\"${database_dir}\"" }
+          $log_dir_option        = empty($database_log_dir) ? { true => '', default => "/SQLUSERDBLOGDIR=\"${database_log_dir}\"" }
+          $backup_dir_option     = empty($backup_dir)         ? { true => '', default => "/SQLBACKUPDIR=\"${backup_dir}\"" }
+          $collation_option      = empty($collation)          ? { true => '', default => "/SQLCOLLATION=\"${collation}\"" }
+          $administrators_option = empty($administrators)     ? { true => "/SQLSYSADMINACCOUNTS=\"${::hostname}\\Administrator\"", default => "/SQLSYSADMINACCOUNTS=\"${administrators}\"" }
+          $options               = "${silent_option} ${features_option} ${enu_option} ${instance_name_option} ${instance_dir_option} ${security_option} ${database_dir_option} ${log_dir_option} ${backup_dir_option} ${collation_option}"
 
           exec {"sqlserver-install-download":
             command  => "((new-object net.webclient).DownloadFile('${sql_source}','${core::cache_dir}/${sql_install}'))",
@@ -113,8 +146,8 @@ class sqlserver(
           }
 
           exec {"sqlserver-install-run":
-            command  => "${core::cache_dir}/${sql_install} /Q /IACCEPTSQLSERVERLICENSETERMS /ACTION=install /FEATURES=SQL,AS,RS,IS,Tools /INSTANCENAME=\"MSSQLSERVER\" /SECURITYMODE=SQL /SAPWD=\"${sa_password}\" /TCPENABLED=1",
-            creates  => "C:/Program Files/Microsoft SQL Server/MSSQL11.MSSQLSERVER/MSSQL/binn/sqlservr.exe",
+            command  => "${core::cache_dir}/${sql_install} /IACCEPTSQLSERVERLICENSETERMS /ACTION=install ${options} /TCPENABLED=1",
+            creates  => "C:/Program Files/Microsoft SQL Server/MSSQL12.MSSQLSERVER/MSSQL/binn/sqlservr.exe",
             cwd      => "${core::cache_dir}",
             provider => windows,
             timeout  => 900,
@@ -128,20 +161,9 @@ class sqlserver(
         'standard':
         {
           # See: http://msdn.microsoft.com/en-us/library/ms144259.aspx#Install
-          validate_array($features)
-          validate_string($instance)
           validate_string($source)
-          if (empty($features)) { fail("Unable to install SQL Server since no features were selected") }
 
-          # TODO: implement other features
-          # With Analysis Services:
-          # setup.exe ... /FEATURES=...,AS,... /ASSYSADMINACCOUNTS="LAB\Administrator"
-          # With Integration Services:
-          # setup.exe ... /FEATURES=...,IS,...
-          # With Reporting Services:
-          # setup.exe ... /FEATURES=...,RS,...
           $features_option = '/FEATURES=SQL'
-
           $instance_name_option  = empty($instance_name)      ? { true => "/INSTANCENAME=\"MSSQLSERVER\"", default => "/INSTANCENAME=\"${instance_name}\"" }
           $instance_dir_option   = empty($instance_dir)       ? { true => '', default => "/INSTANCEDIR=\"${instance_dir}\"" }
           $license_option        = empty($license)            ? { true => '', default => "/PID=\"${license}\"" }
@@ -209,3 +231,27 @@ class sqlserver(
 
   #TODO: Open the firewall for the TCP connection to SQL Server
 }
+          # SQL Server 2014 Express
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe'
+
+          # SQL Server 2014 Express with tools
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAndTools%2064BIT/SQLEXPRWT_x64_ENU.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/B/8/5/B856F507-1B8A-4A5F-BCBF-ABEE9C70CA84/ExpressAndTools%2064BIT/SQLEXPRWT_x64_FRA.exe'
+
+          # SQL Server 2014 Management Studio (64b)
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/1/C/9/1C95EAB0-F98C-4039-8402-4D7A84D9B290/MgmtStudio%2064BIT/SQLManagementStudio_x64_JPN.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/B/8/5/B856F507-1B8A-4A5F-BCBF-ABEE9C70CA84/MgmtStudio%2064BIT/SQLManagementStudio_x64_FRA.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/9/9/B/99BB8518-C818-42EF-A9AA-1A06E4AC1DC6/MgmtStudio%2064BIT/SQLManagementStudio_x64_DEU.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/C/A/3/CA36A732-59EC-4CEA-971A-0269B992C82A/MgmtStudio%2064BIT/SQLManagementStudio_x64_ESN.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/C/5/A/C5ACFA2B-9DB0-44F3-BD2F-BBC567987C82/MgmtStudio%2064BIT/SQLManagementStudio_x64_CHS.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/5/5/E/55EA61C3-4CED-455F-B09F-67608D27BEB6/MgmtStudio%2064BIT/SQLManagementStudio_x64_CHT.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/5/9/9/5995FEA5-97E8-4A01-BDFB-78E27F4873AC/MgmtStudio%2064BIT/SQLManagementStudio_x64_KOR.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/0/1/5/015567C0-E851-4AC6-964F-9BBA9B31D6BC/MgmtStudio%2064BIT/SQLManagementStudio_x64_PTB.exe'
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/4/E/3/4E38FD5A-8859-446F-8C58-9FC70FE82BB1/MgmtStudio%2064BIT/SQLManagementStudio_x64_RUS.exe'
+
+          # SQL Server 2014 with Advanced Services
+          #$sql_source  = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAdv%2064BIT/SQLEXPRADV_x64_ENU.exe'
+
+          # SQL Express Server 2012
+          #$sql_source  = 'http://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPR_x64_ENU.exe'
